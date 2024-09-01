26905d6803751f88655407d320f77dcbd2d38d05
leonarddeutgen <leonard.deutgen@medieinstitutet.se>
2024-09-01 23:43:39 +0200

    :boom: Added theme Mode!

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,13 +1,28 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
+import { useState } from "react";
+import { ITheme, ThemeContext, themes } from "../context/ThemeContext";
 
 export const Layout = () => {
+  const [theme, setTheme] = useState<ITheme>(themes.dark);
+
+  const toogleTheme = () => {
+    setTheme(theme.name === themes.dark.name ? themes.light : themes.dark);
+  };
+
   return (
     <>
-      <main className="appContainer">
-        <Navbar></Navbar>
-        <Outlet></Outlet>
-      </main>
+      <ThemeContext.Provider value={theme}>
+        <main
+          className="appContainer"
+          style={{
+            backgroundColor: theme.background,
+          }}
+        >
+          <Navbar toggleTheme={toogleTheme}></Navbar>
+          <Outlet></Outlet>
+        </main>
+      </ThemeContext.Provider>
     </>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,24 @@
 import { useNavigate } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
-export const Navbar = () => {
+interface NavigationProps {
+  toggleTheme: () => void;
+}
+
+export const Navbar = ({ toggleTheme }: NavigationProps) => {
   const navigate = useNavigate();
   return (
     <>
       <section className="navBarContainer">
-        <h3 className="navBarContainer--btn" onClick={() => navigate("/")}>
-          DLC
-        </h3>
+        <DropdownButton id="dropdown-basic-button" title="Theme Mode">
+          <Dropdown.Item href="#/action-1" onClick={() => toggleTheme()}>
+            Light Mode
+          </Dropdown.Item>
+          <Dropdown.Item href="#/action-2" onClick={() => toggleTheme()}>
+            Dark Mode
+          </Dropdown.Item>
+        </DropdownButton>
 
         {/* <div className="menu">
           <ul className="menu--list">
