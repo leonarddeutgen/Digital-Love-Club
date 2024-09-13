6b2eb252f1a0662ff37e9d4f7573120ded3ef0aa
leonarddeutgen <leonard.deutgen@medieinstitutet.se>
2024-09-13 11:37:51 +0200

    :closed_lock_with_key: Added env

--- src/services/youtubeService.ts
+++ src/services/youtubeService.ts
@@ -2,8 +2,10 @@ import { IVideo } from "../models/IVideo";
 import { getData } from "./serviceBase";
 
 export const getVideos = async (): Promise<IVideo> => {
-  const url =
-    "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=UUuZuBuctUovKIH9JnSidSRA&key=AIzaSyD8cuiWZMTO_72m5_rwMlVNownS1cOWuBs";
+  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=15&playlistId=UUuZuBuctUovKIH9JnSidSRA&key=${
+    import.meta.env.VITE_REACT_APP_API_KEY
+  }`;
   const data = await getData<IVideo>(url);
+
   return data;
 };
